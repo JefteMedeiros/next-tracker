ad641e2a1e37fa1983334fc62cf6bf3214cc5827
Jefté Medeiros <jefte.medeiros8@gmail.com>
2022-12-20 10:57:01 -0500

    build(added-testing-libraries): cypress and jest were added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
